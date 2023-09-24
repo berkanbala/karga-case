278df8a7d98adbcaa90f2ee600c4a16621f6dcc2
berkan bala <berkan.balaa@gmail.com>
2023-09-24 10:21:56 +0300

    env commit

--- src/custom/pages/dashboard/dashboard.jsx
+++ src/custom/pages/dashboard/dashboard.jsx
@@ -11,6 +11,9 @@ import ImagePageAd from "@/common/media/images/image5.png";
 import Image from "next/image";
 import styles from "./dashboard.module.scss";
 import ImageSlider from "@/custom/components/imageSlider/imageSlider";
+import "dotenv/config";
+
+require("dotenv").config();
 
 export default function Dashboard() {
   const stylesImageAd = {
